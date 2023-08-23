90dd91a722dd19cec72773b82d89c9d5ac5e9f91
Valter_Vitor <126479682+DEVitor77@users.noreply.github.com>
2023-08-23 10:12:33 -0300

    Add files via upload

---
+++ InsertDados.sql
@@ -0,0 +1,13 @@
+use sucos;
+
+INSERT INTO tbProdutos (
+PRODUTO,
+NOME,
+EMBALAGEM,
+TAMANHO,
+SABOR,
+PRECO_LISTA) VALUES ( 
+
+'104107','Light - 350 ml - Melância','Lata','350 ml', 'melância', 4.56);
+
+select * from tbProdutos;
